3eb7a41a128880766664b677dc511ac5af99a029
bass12345-dev <manabobasil@gmail.com>
2024-10-11 00:29:49 +0800

    update

--- src/app/amora_media_management/app-admin/pages/pdf-generator/sections/generate-form/generate-form.component.ts
+++ src/app/amora_media_management/app-admin/pages/pdf-generator/sections/generate-form/generate-form.component.ts
@@ -27,6 +27,11 @@ export class GenerateFormComponent {
   plugins     : GUIDELINES[] = [];
   effects     : GUIDELINES[] = [];
   positioning : GUIDELINES[] = [];
+  //Fonts
+  fonts       : GUIDELINES[] = [];
+  title_style : GUIDELINES[] = [];
+  title_effects : GUIDELINES[] = [];
+  //Custom
   custom      : GUIDELINES[] = [];
   guideline_is_hidden : boolean = true;
   spinner     : boolean = false;
@@ -38,10 +43,17 @@ export class GenerateFormComponent {
 
   getGuidelines(){
     this.api.QueryGuidelines().subscribe((response:any) => {
-      this.plugins = response.premiere_pro_plugins;
-      this.effects = response.after_effects_plugins;
-      this.positioning = response.positioning_style;
-      this.custom = response.custom
+
+      this.plugins      = response.premiere_pro_plugins;
+      this.effects      = response.after_effects_plugins;
+      this.positioning  = response.positioning_style;
+
+      //Fonts
+      this.fonts       = response.fonts;
+      this.title_style = response.title_style
+      this.title_effects = response.title_effects;
+      //Custom
+      this.custom = response.custom;
       this.guideline_is_hidden = false;
       this.spinner = true;
     },(error) =>{
